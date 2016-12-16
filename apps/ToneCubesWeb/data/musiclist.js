80fb9b94d9f4f913c49c2a67cca7354850d536c1
46neko <46neko.mod@gmail.com>
2016-12-16 19:17:55 +0900

    listtest

--- apps/ToneCubesWeb/data/musiclist.js
+++ apps/ToneCubesWeb/data/musiclist.js
@@ -1,1 +1,1 @@
-﻿var musics=[["On my way v2 Free Short ver.","https://46neko.github.io/46nekoProject/apps/ToneCubesWeb/musics/On_my_way_v2_Free_Short_ver..tcd"]];
+﻿var musics=[["On my way v2 Free Short ver.","https://46neko.github.io/46nekoProject/apps/ToneCubesWeb/musics/On_my_way_v2_Free_Short_ver..tcd"],["On my way v2 Free Short ver.","https://46neko.github.io/46nekoProject/apps/ToneCubesWeb/musics/On_my_way_v2_Free_Short_ver..tcd"]];
