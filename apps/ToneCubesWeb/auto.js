4ff48ab6aebbb331f45930776a71b9e346a3f7d9
46neko <46neko.mod@gmail.com>
2016-12-18 11:20:21 +0900

    点数計算修正

---
+++ apps/ToneCubesWeb/auto.js
@@ -0,0 +1,5 @@
+﻿//modTick内に記述
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][0]==14)tone(0,array[Math.ceil(document.getElementById('audio').currentTime*20)][0]);
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][1]==14)tone(1,array[Math.ceil(document.getElementById('audio').currentTime*20)][1]);
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][2]==14)tone(2,array[Math.ceil(document.getElementById('audio').currentTime*20)][2]);
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][3]==14)tone(3,array[Math.ceil(document.getElementById('audio').currentTime*20)][3]);
