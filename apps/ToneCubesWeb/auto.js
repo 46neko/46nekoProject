09ccfb7ce97f35afbcbc2dfcfff80e2498096334
46neko <46neko.mod@gmail.com>
2016-12-18 12:34:37 +0900

    修正

--- apps/ToneCubesWeb/auto.js
+++ apps/ToneCubesWeb/auto.js
@@ -1,5 +1,5 @@
 ﻿//modTick内に記述
-if(array[Math.ceil(document.getElementById('audio').currentTime*20)][0]==14)tone(0,array[Math.ceil(document.getElementById('audio').currentTime*20)][0]);
-if(array[Math.ceil(document.getElementById('audio').currentTime*20)][1]==14)tone(1,array[Math.ceil(document.getElementById('audio').currentTime*20)][1]);
-if(array[Math.ceil(document.getElementById('audio').currentTime*20)][2]==14)tone(2,array[Math.ceil(document.getElementById('audio').currentTime*20)][2]);
-if(array[Math.ceil(document.getElementById('audio').currentTime*20)][3]==14)tone(3,array[Math.ceil(document.getElementById('audio').currentTime*20)][3]);
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][0]==14)tone(0,14);
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][1]==14)tone(1,14);
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][2]==14)tone(2,14);
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][3]==14)tone(3,14);
