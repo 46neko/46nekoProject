95044103cf306e9ec330f148b3e19c0251f3eaa4
46neko <46neko.mod@gmail.com>
2016-12-24 16:28:51 +0900

    UP

---
+++ apps/ToneCubesWeb/auto.js
@@ -0,0 +1,5 @@
+﻿//modTick-if(run)内に記述することで自動演奏になります。
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][0]==14)tone(0,14);
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][1]==14)tone(1,14);
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][2]==14)tone(2,14);
+if(array[Math.ceil(document.getElementById('audio').currentTime*20)][3]==14)tone(3,14);
---
+++ apps/ToneCubesWeb/data/musiclist.js
@@ -0,0 +1,1 @@
+﻿var musics=[["On my way v2 Free Short ver.","https://46neko.github.io/46nekoProject/apps/ToneCubesWeb/musics/On_my_way_v2_Free_Short_ver..tcd"]];
