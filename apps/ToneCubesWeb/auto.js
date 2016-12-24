28b50587e2585d35144793ddd05a0c00deeffdab
46neko <46neko.mod@gmail.com>
2016-12-24 16:26:46 +0900

    TCEリンク追加

--- apps/ToneCubesWeb/auto.js
+++ apps/ToneCubesWeb/auto.js
@@ -1,4 +1,4 @@
-﻿//modTick内に記述
+﻿//modTick-if(run)内に記述することで自動演奏になります。
 if(array[Math.ceil(document.getElementById('audio').currentTime*20)][0]==14)tone(0,14);
 if(array[Math.ceil(document.getElementById('audio').currentTime*20)][1]==14)tone(1,14);
 if(array[Math.ceil(document.getElementById('audio').currentTime*20)][2]==14)tone(2,14);
